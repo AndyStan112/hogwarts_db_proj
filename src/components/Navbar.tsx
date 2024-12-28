f545e1b292bd06ff46d8e1f1cebf1b0d72b1dedd
AndyStan112 <stanica.andrei28@gmail.com>
2024-12-28 21:10:30 +0200

    fix blog link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ export default async function Navbar() {
           { href: "/", label: "HOME" },
           { href: "/about", label: "ABOUT" },
           { href: "/houses", label: "HOUSES" },
-          { href: "/blog", label: "BLOG" },
+          { href: "/blogs", label: "BLOG" },
           { href: "/contact", label: "CONTACT" },
         ].map((link) => (
           <li key={link.href}>
