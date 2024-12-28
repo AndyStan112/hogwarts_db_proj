6f5d002b769341acf83b8a04597ee5108de59a81
Enilesi <miriam.iseline@gmail.com>
2024-12-28 16:40:11 +0200

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+      <footer className="text-center py-6 bg-amber-50 w-full">
+        <p className="text-gray-600">
+          Made with <span className="text-red-500">&hearts;</span> by M.Iseline
+        </p>
+        <p className="text-gray-600">and S.Andy</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
