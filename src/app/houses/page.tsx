8d32472056eab41600552bbbfdedafd72f6f12d0
AndyStan112 <stanica.andrei28@gmail.com>
2025-01-05 19:49:35 +0200

    rename and link to houses

--- src/app/houses/page.tsx
+++ src/app/houses/page.tsx
@@ -22,42 +22,41 @@ const page = () => {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
             {[
               {
-                id: "gryffindor",
+                id: "Gryffindor",
                 img: "/images/Gryffindor_Crest.jpeg",
                 name: "Gryffindor",
                 desc: "The bravest of wizards are placed right in this house",
               },
               {
-                id: "ravenclaw",
+                id: "Ravenclaw",
                 img: "/images/RavenclawCrest.jpeg",
                 name: "Ravenclaw",
                 desc: "For those with an insatiable thirst for knowledge, this house is like a dream",
               },
               {
-                id: "hufflepuff",
+                id: "Hufflepuff",
                 img: "/images/HufflepuffCrest1.jpeg",
                 name: "Hufflepuff",
                 desc: "Determined, loyal, and passionate is what a true Hufflepuff is like!",
               },
               {
-                id: "slytherin",
+                id: "Slytherin",
                 img: "/images/SlytherinCrest.jpeg",
                 name: "Slytherin",
                 desc: "Ambitious and stubborn like a bull, but sharp-tongued like a snake, this is a Slytherin's essence",
               },
             ].map((house) => (
-              <div
-                key={house.id}
-                className="bg-amber-100 rounded-lg shadow-lg p-4 text-center"
-              >
-                <img
-                  src={house.img}
-                  alt={`${house.name} Crest`}
-                  className="w-3/4 mx-auto rounded-lg border-4 border-amber-600 mb-4"
-                />
-                <h3 className="font-semibold text-lg">{house.name}</h3>
-                <p className="text-gray-700 mt-2">{house.desc}</p>
-              </div>
+              <a className="bg-amber-100 rounded-lg shadow-lg p-4 text-center" href = {`/houses/${house.id}`} key={house.id}>
+             
+              <img
+                src={house.img}
+                alt={`${house.name} Crest`}
+                className="w-3/4 mx-auto rounded-lg border-4 border-amber-600 mb-4"
+              />
+              <h3 className="font-semibold text-lg">{house.name}</h3>
+              <p className="text-gray-700 mt-2">{house.desc}</p>
+            </a>
+             
             ))}
           </div>
 
